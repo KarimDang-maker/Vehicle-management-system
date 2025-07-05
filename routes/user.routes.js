6dd11ff0c75d0664dd5e336f508ff0051f45bb9b
ulrichakongo <ulrichakongo@gmail.com>
2025-07-05 06:37:24 +0100

    API USERS

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, updateUser } = require('../controllers/user.controller');
+const { createUser, updateUser, getAllUsers } = require('../controllers/user.controller');
 const auth = require('../middlewares/auth');
 
 router.post('/', createUser);
 router.put('/:id', auth, updateUser);
+router.get('/', getAllUsers); // <-- Ajoute cette ligne
 
 module.exports = router;
